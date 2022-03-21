37f66bcbd977d8e2a68ad1e4f3b36f69632bbe20
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-03-21 10:55:11 +0900

    build: integrate prettier

--- pages/api/neo4j.ts
+++ pages/api/neo4j.ts
@@ -1,10 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { getNeo4jDriver } from '../../src';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const db = getNeo4jDriver();
   const info = await db.getServerInfo();
 
